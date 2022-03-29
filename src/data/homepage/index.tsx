5ac540a05cf70eaab8639d512a8ddea714c37bef
Francesco Scatassa <fscatassa@photosi.com>
2022-03-29 22:28:58 +0200

    added homepage gallery

--- src/data/homepage/index.tsx
+++ src/data/homepage/index.tsx
@@ -1,6 +1,12 @@
-import Img1 from '../../images/homepage/1.jpeg'
-import Img2 from '../../images/homepage/2.jpeg'
-import Img3 from '../../images/homepage/3.jpeg'
+import Img1 from '../../images/homepage/01-min.jpg'
+import Img2 from '../../images/homepage/02-min.jpg'
+import Img3 from '../../images/homepage/03-min.jpg'
+import Img4 from '../../images/homepage/04-min.jpg'
+import Img5 from '../../images/homepage/05-min.jpg'
+import Img6 from '../../images/homepage/06-min.jpg'
+import Img7 from '../../images/homepage/07-min.jpg'
+import Img8 from '../../images/homepage/08-min.jpg'
+import Img9 from '../../images/homepage/09-min.jpg'
 
 export const GALLERY_IMAGES = [
     {
@@ -14,5 +20,29 @@ export const GALLERY_IMAGES = [
     {
         src: Img3,
         alt: '3'
+    },
+    {
+        src: Img4,
+        alt: '4'
+    },
+    {
+        src: Img5,
+        alt: '5'
+    },
+    {
+        src: Img6,
+        alt: '6'
+    },
+    {
+        src: Img7,
+        alt: '7'
+    },
+    {
+        src: Img8,
+        alt: '8'
+    },
+    {
+        src: Img9,
+        alt: '9'
     }
 ]
