321faa7c7460433ea893cbb44b5956c4576402a0
frascata <francesco.scatassa@gmail.com>
2023-09-28 00:01:17 +0200

    update cookie dependencies

--- src/components/CookieLawBanner.tsx
+++ src/components/CookieLawBanner.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-// import {CookieNotice} from "gatsby-cookie-notice";
+import {CookieNotice} from "gatsby-cookie-notice";
 
 import '../styles/components/cookie-law-banner.scss'
 
+// Update with: https://medium.com/geekculture/privacy-friendly-analytics-in-gatsbyjs-daaa2935518f
 export const CookieLawBanner = () => {
-    return <div/>
-    // return <CookieNotice
-    //     backgroundWrapperClasses={'cookie-law-banner'}
-    //     acceptButtonText={'OK'}
-    //     declineButton={false}
-    //     personalizeButtonEnable={false}
-    // >
-    //     <div>
-    //         Il sito utilizza cookie tecnici per il corretto funzionamento delle pagine web e cookie di tracciamento anonimizzati per la produzione di statistiche sugli accessi che non necessitano
-    //         del preventivo consenso dell'utente. Non sono utilizzati cookie di terze parti per la fruizione dei contenuti media.
-    //     </div>
-    // </CookieNotice>
+    return <CookieNotice
+        backgroundWrapperClasses={'cookie-law-banner'}
+        acceptButtonText={'OK'}
+        declineButton={false}
+        personalizeButtonEnable={false}
+    >
+        <div>
+            Il sito utilizza cookie tecnici per il corretto funzionamento delle pagine web e cookie di tracciamento anonimizzati per la produzione di statistiche sugli accessi che non necessitano
+            del preventivo consenso dell'utente. Non sono utilizzati cookie di terze parti per la fruizione dei contenuti media.
+        </div>
+    </CookieNotice>
 }
