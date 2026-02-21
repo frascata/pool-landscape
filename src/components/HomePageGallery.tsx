34457556a9fa99f1e107cf30d6307fdb23409f13
frascata <francesco.scatassa@gmail.com>
2026-02-21 16:10:54 +0100

    chore: update dependencies, migrate to Node 22 and Dart Sass modules

--- src/components/HomePageGallery.tsx
+++ src/components/HomePageGallery.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
-import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import React, { useEffect } from 'react'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 
-import {GatsbyImage} from 'gatsby-plugin-image'
+import { GatsbyImage } from 'gatsby-plugin-image'
 // Import Swiper styles
 import 'swiper/css'
 import "swiper/css/effect-fade";
@@ -10,25 +10,25 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 // import required modules
 
-import {Navigation, Pagination, Keyboard, Autoplay, EffectFade} from 'swiper'
+import { Navigation, Pagination, Keyboard, Autoplay, EffectFade } from 'swiper/modules'
 import '../styles/homepage/gallery.scss'
 
-export const SwiperHomeGallery = ({images}) => {
+export const SwiperHomeGallery = ({ images }) => {
     return <Swiper spaceBetween={30}
-                   effect={"fade"}
-                   navigation={true}
-                   pagination={{
-                       clickable: true,
-                   }}
-                   keyboard={{
-                       enabled: true,
-                   }}
-                   autoplay={true}
-                   modules={[Autoplay, EffectFade, Navigation, Pagination, Keyboard]}
-                   className="mySwiper">
+        effect={"fade"}
+        navigation={true}
+        pagination={{
+            clickable: true,
+        }}
+        keyboard={{
+            enabled: true,
+        }}
+        autoplay={true}
+        modules={[Autoplay, EffectFade, Navigation, Pagination, Keyboard]}
+        className="mySwiper">
         {images.map((image, index) => (
             <SwiperSlide key={index}>
-                <GatsbyImage image={image.gatsbyImageData} alt={image.alt}/>
+                <GatsbyImage image={image.gatsbyImageData} alt={image.alt} />
             </SwiperSlide>
         ))}
     </Swiper>
